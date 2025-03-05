b6462b3e090e048797793a6411d6d3eebcf5aa4f
Sébastien Ballen Castaneda <sebastian.ballenc@gmail.com>
2025-03-05 10:52:57 -0500

    feat: axios config & client service

---
+++ src/api/client.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+})
+
+apiClient.interceptors.response.use(
+  response => response,
+  error => {
+    console.error('API Error:', error.response?.data || error.message);
+    throw error;
+  }
+);
